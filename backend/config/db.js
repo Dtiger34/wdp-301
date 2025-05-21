3e60b0dcc9a1b050dff21c98139554579c5dfb39
Dtiger34 <duongleduongle01@gmail.com>
2025-05-21 15:05:56 +0700

    start

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+dotenv.config(); // Cho phép đọc các biến môi trường từ .env
+
+const connectionDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connect to MongoDB successfully!");
+    } catch (error) {
+        console.error("Connection to MongoDB failse: " + error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectionDB;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const connectionDB = require('./config/db');
 
 app.get('/', (req, res) => {
   res.send('Hello from backend!');
 });
-
+const PORT = process.env.PORT || 9999;
 app.listen(PORT, () => {
   console.log(`Backend is running at http://localhost:${PORT}`);
 });
