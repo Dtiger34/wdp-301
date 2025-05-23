417972629aafe4f19daa42784f00fdc71b8f2317
Dtiger34 <duongleduongle01@gmail.com>
2025-05-23 19:09:15 +0700

    add

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
+
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -13,16 +14,19 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    required: true,
-    unique: true,
   }
 });
-// // Hash password before saving
-// userSchema.pre('save', async function (next) {
-//   if (this.isModified('password')) {
-//     this.password = await bcrypt.hash(this.password, 10);
-//   }
-//   next();
-// });
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
--- backend/server.js
+++ backend/server.js
@@ -16,27 +16,7 @@ app.use(cors());
 app.use(urlencoded({extended:true}));
 
 
-
-
-app.post('/register', async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    // Check if user already exists
-    const existingUser = await User.findOne({ username });
-    if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
-    }
-
-    // Create new user
-    const newUser = new User({ username, password });
-    await newUser.save();
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
-
+app.use('/api/v1', require('./routes/authRoute'));
 
 
 
