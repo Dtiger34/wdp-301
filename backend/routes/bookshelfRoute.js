237900a9669704516a43637310431d75d75840ce
Hoang Nh <hoangnguyenhuu11122003@gmail.com>
2025-06-15 21:15:25 +0700

    Bookshelf

---
+++ backend/routes/bookshelfRoute.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const bookshelfController = require('../controller/bookshelfController');
+
+// Bookshelf routes
+router.post('/', bookshelfController.createBookshelf);
+router.get('/', bookshelfController.getAllBookshelves);
+router.get('/:id', bookshelfController.getBookshelfById);
+router.put('/:id', bookshelfController.updateBookshelf);
+router.delete('/:id', bookshelfController.deleteBookshelf);
+
+module.exports = router;
