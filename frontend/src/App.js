296438012fc4dfa8c0d61a7ed5d36ece4672d480
Hoang Nh <hoangnguyenhuu11122003@gmail.com>
2025-06-23 00:05:15 +0700

    category fe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,11 @@ import UpdateBook from './pages/staff/UpdateBook';
 import BookShelf from './pages/staff/BookShelf';
 import AddBookshelf from './pages/staff/AddBookshelf';
 import UpdateBookshelf from './pages/staff/UpdateBookshelf';
+
+import ViewCategoryList from './pages/staff/ViewCategoryList';
+import AddCategory from './pages/staff/AddCategory';
+import UpdateCategory from './pages/staff/UpdateCategory';
+
 function App() {
   return (
     <Router>
@@ -17,9 +22,16 @@ function App() {
         <Route path="/staff/view-books" element={<ViewBookList />} />
         <Route path="/staff/add-book" element={<AddBook />} />
         <Route path="/staff/update-book/:id" element={<UpdateBook />} />
+
+        {/* CRUD Bookshelf */}
         <Route path="/staff/bookshelf" element={<BookShelf />} />
         <Route path="/staff/add-bookshelf" element={<AddBookshelf />} />
         <Route path="/staff/update-bookshelf/:id" element={<UpdateBookshelf />} />
+
+        {/* CRUD Category */}
+        <Route path="/staff/ViewCategoryList" element={<ViewCategoryList />} />
+        <Route path="/staff/AddCategory" element={<AddCategory />} />
+        <Route path="/staff/UpdateCategory" element={<UpdateCategory />} />
       </Routes>
     </Router>
   );
